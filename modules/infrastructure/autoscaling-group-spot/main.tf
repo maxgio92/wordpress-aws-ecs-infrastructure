f90c17fb63061f10c6181740adab064d301f4721
Massimiliano Giovagnoli <massimiliano.giovagnoli@innoteam.it>
2019-08-10 16:30:22 +0200

    Add module to manage Autoscaling Group with mixed instance lifecycles

---
+++ modules/infrastructure/autoscaling-group-spot/main.tf
@@ -0,0 +1,62 @@
+locals {
+  # Ugly workaround to: https://github.com/hashicorp/terraform/issues/11566
+  launch_template_id = "${var.custom_instance_profile ?
+    join(" ", aws_launch_template.custom_instance_profile.*.id) :
+    join(" ", aws_launch_template.main.*.id)}"
+}
+
+resource "aws_launch_template" "main" {
+  count         = "${var.custom_instance_profile ? 0 : 1}"
+  name_prefix   = "main"
+  image_id      = "${var.image_id}"
+  instance_type = "${var.instance_types[0]}"
+  user_data     = "${base64encode(var.instance_user_data)}"
+
+  vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
+}
+
+resource "aws_launch_template" "custom_instance_profile" {
+  count         = "${var.custom_instance_profile ? 1 : 0}"
+  name_prefix   = "main"
+  image_id      = "${var.image_id}"
+  instance_type = "${var.instance_types[0]}"
+
+  iam_instance_profile {
+    arn = "${var.instance_profile_arn}"
+  }
+
+  user_data = "${base64encode(var.instance_user_data)}"
+
+  vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
+}
+
+resource "aws_autoscaling_group" "main" {
+  desired_capacity    = "${var.desired_capacity}"
+  max_size            = "${var.max_size}"
+  min_size            = "${var.min_size}"
+  vpc_zone_identifier = ["${var.vpc_subnet_ids}"]
+
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = "${local.launch_template_id}"
+      }
+
+      override {
+        instance_type = "${var.instance_types[0]}"
+      }
+
+      override {
+        instance_type = "${var.instance_types[1]}"
+      }
+    }
+
+    instances_distribution {
+      on_demand_base_capacity                  = "${var.on_demand_base_capacity}"
+      on_demand_percentage_above_base_capacity = "${var.on_demand_percentage_above_base_capacity}"
+      spot_allocation_strategy                 = "${var.spot_allocation_strategy}"
+      spot_instance_pools                      = "${var.spot_instance_pools}"
+      spot_max_price                           = "${var.spot_max_price}"
+    }
+  }
+}
---
+++ modules/infrastructure/autoscaling-group-spot/variables.tf
@@ -0,0 +1,99 @@
+variable "desired_capacity" {
+  default     = 2
+  description = "The number of Amazon EC2 instances that should be running in the group"
+}
+
+variable "max_size" {
+  default     = 2
+  description = "The maximum size of the auto scale group"
+}
+
+variable "min_size" {
+  default     = 2
+  description = "The minimum size of the auto scale group"
+}
+
+variable "vpc_subnet_ids" {
+  type        = "list"
+  description = "A list of subnet IDs to launch resources in"
+}
+
+variable "image_id" {
+  description = "The AMI ID of the autoscaling group's instances"
+}
+
+variable "instance_types" {
+  type = "list"
+
+  description = <<EOF
+  A list of 2 instance types of the autoscaling group.
+  The list is prioritized: instances at the top of the list
+  will be used in preference to those lower down.
+  EOF
+}
+
+variable "custom_instance_profile" {
+  default     = true
+  description = "Whether to launch instances with custom IAM instance profile"
+}
+
+variable "instance_profile_arn" {
+  default     = ""
+  description = "The IAM instance profile ARN to launch the autoscaling group's instances with"
+}
+
+variable "instance_user_data" {
+  default     = ""
+  description = "The user data to provide when launching the autoscaling group's instances"
+}
+
+variable "vpc_security_group_ids" {
+  type = "list"
+
+  description = <<EOF
+  The list of VPC security group IDs to associate
+  the autoscaling group's instances with.
+  EOF
+}
+
+variable "on_demand_base_capacity" {
+  default = 0
+
+  description = <<EOF
+  Absolute minimum amount of desired capacity
+  that must be fulfilled by on-demand instances.
+  EOF
+}
+
+variable "on_demand_percentage_above_base_capacity" {
+  default = 100
+
+  description = <<EOF
+  Percentage split between on-demand and Spot
+  instances above the base on-demand capacity.
+  EOF
+}
+
+variable "spot_allocation_strategy" {
+  default     = "lowest-price"
+  description = "How to allocate capacity across the Spot pools"
+}
+
+variable "spot_instance_pools" {
+  default = 2
+
+  description = <<EOF
+  Number of Spot pools per availability zone to allocate capacity.
+  EC2 Auto Scaling selects the cheapest Spot pools and evenly allocates
+  Spot capacity across the number of Spot pools that you specify.
+  EOF
+}
+
+variable "spot_max_price" {
+  default = ""
+
+  description = <<EOF
+  Maximum price per unit hour that the user is willing to pay for
+  the Spot instances.
+  EOF
+}
