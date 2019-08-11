33c70c1a2b3a9703532884f2ddd7b5199049c60d
Massimiliano Giovagnoli <massimiliano.giovagnoli@innoteam.it>
2019-08-11 12:41:16 +0200

    Upgrade ECR repository modules

--- repositories.tf
+++ repositories.tf
@@ -1,19 +1,19 @@
 data "aws_caller_identity" "current" {}
 
 module "wordpress_nginx_ecr_repository" {
-  source                       = "github.com/maxgio92/terraform-aws-ecr-repository?ref=1.0.0"
+  source                       = "github.com/maxgio92/terraform-aws-ecr-repository?ref=1.2.0"
   name                         = "${var.app_name}/nginx"
   aws_accounts_allowed_to_pull = ["${data.aws_caller_identity.current.account_id}"]
 }
 
 module "wordpress_php_fpm_ecr_repository" {
-  source                       = "github.com/maxgio92/terraform-aws-ecr-repository?ref=1.0.0"
+  source                       = "github.com/maxgio92/terraform-aws-ecr-repository?ref=1.2.0"
   name                         = "${var.app_name}/php-fpm"
   aws_accounts_allowed_to_pull = ["${data.aws_caller_identity.current.account_id}"]
 }
 
 module "wordpress_php_cli_ecr_repository" {
-  source                       = "github.com/maxgio92/terraform-aws-ecr-repository?ref=1.0.0"
+  source                       = "github.com/maxgio92/terraform-aws-ecr-repository?ref=1.2.0"
   name                         = "${var.app_name}/php-cli"
   aws_accounts_allowed_to_pull = ["${data.aws_caller_identity.current.account_id}"]
 }
