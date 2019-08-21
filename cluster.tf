728c81bf43f94cd1592419ec4206b9d8013110c3
Massimiliano Giovagnoli <massimiliano.giovagnoli@innoteam.it>
2019-08-21 13:56:22 +0200

    Refactor ECS cluster settings

--- cluster.tf
+++ cluster.tf
@@ -1,39 +1,19 @@
-# ECS Cluster (resource)
+# -----------------------------------------------------------------------
+# ECS cluster
+# -----------------------------------------------------------------------
+
+# ECS cluster
+
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "${var.app_name}-${var.env_name}-cluster"
 }
 
-# Autoscaling Group (module)
-module "ecs_autoscaling_group" {
-  source = "./modules/infrastructure/autoscaling-group-spot"
+# -----------------------------------------------------------------------
+# Autoscaling group
+# -----------------------------------------------------------------------
 
-  image_id = "${data.aws_ami.latest_ecs_optimized.id}"
+# ECS-optimized AMI
 
-  instance_profile_arn                     = "${module.ecs_instance_profile.arn}"
-  instance_user_data                       = "${var.ecs_cluster_mount_efs ? data.template_file.ecs_instance_user_data_efs.rendered : data.template_file.ecs_instance_user_data.rendered}"
-  instance_types                           = ["${var.ecs_cluster_instance_types}"]
-  instance_key_name                        = "${var.ecs_cluster_instance_key_name}"
-  on_demand_percentage_above_base_capacity = 50
-  vpc_subnet_ids                           = ["${module.vpc.private_subnet_ids}"]
-  vpc_security_group_ids                   = ["${aws_security_group.ecs_cluster.id}"]
-}
-
-# AMI Instance Profile for ECS (module)
-module "ecs_instance_profile" {
-  source = "./modules/infrastructure/ecs-instance-profile"
-  name   = "ecs-instance-profile"
-}
-
-# Instance User Data for ECS (data)
-data "template_file" "ecs_instance_user_data" {
-  template = "${file("./templates/user-data.sh")}"
-
-  vars {
-    cluster_name = "${aws_ecs_cluster.ecs_cluster.name}"
-  }
-}
-
-# ECS-optimized AMI (data)
 data "aws_ami" "latest_ecs_optimized" {
   most_recent = true
   owners      = ["amazon"]
@@ -49,7 +29,25 @@ data "aws_ami" "latest_ecs_optimized" {
   }
 }
 
-# ECS Cluster's Security Groups (resources)
+# AMI Instance Profile for ECS
+
+module "ecs_instance_profile" {
+  source = "./modules/infrastructure/ecs-instance-profile"
+  name   = "ecs-instance-profile"
+}
+
+# Instance user data for ECS
+
+data "template_file" "ecs_instance_user_data" {
+  template = "${file("./templates/user-data.sh")}"
+
+  vars {
+    cluster_name = "${aws_ecs_cluster.ecs_cluster.name}"
+  }
+}
+
+# Security group
+
 resource "aws_security_group" "ecs_cluster" {
   name        = "${var.app_name}-${var.env_name}-ecs-cluster"
   description = "${var.app_name} ${var.env_name} ECS cluster"
@@ -75,4 +73,24 @@ resource "aws_security_group_rule" "ecs_cluster_from_itself" {
   source_security_group_id = "${aws_security_group.ecs_cluster.id}"
 
   security_group_id = "${aws_security_group.ecs_cluster.id}"
+}
+
+# Autoscaling group
+
+module "ecs_autoscaling_group" {
+  source = "./modules/infrastructure/autoscaling-group-spot"
+
+  image_id = "${data.aws_ami.latest_ecs_optimized.id}"
+
+  instance_profile_arn = "${module.ecs_instance_profile.arn}"
+
+  instance_user_data = "${var.ecs_cluster_mount_efs ?
+    data.template_file.ecs_instance_user_data_efs.rendered :
+    data.template_file.ecs_instance_user_data.rendered}"
+
+  instance_types                           = ["${var.ecs_cluster_instance_types}"]
+  instance_key_name                        = "${var.ecs_cluster_instance_key_name}"
+  on_demand_percentage_above_base_capacity = 50
+  vpc_subnet_ids                           = ["${module.vpc.private_subnet_ids}"]
+  vpc_security_group_ids                   = ["${aws_security_group.ecs_cluster.id}"]
 }
