4493b84538fb8d1f127b6e740112f8a4eec69e47
Massimiliano Giovagnoli <massimiliano.giovagnoli@innoteam.it>
2019-08-21 14:06:10 +0200

    Refactor ECR repositories settings

--- repositories.tf
+++ repositories.tf
@@ -1,4 +1,10 @@
+# -----------------------------------------------------------------------
+# Application ECR repositories
+# -----------------------------------------------------------------------
+
 data "aws_caller_identity" "current" {}
+
+# Wordpress Nginx
 
 module "wordpress_nginx_ecr_repository" {
   source                       = "github.com/maxgio92/terraform-aws-ecr-repository?ref=1.2.0"
@@ -6,11 +12,15 @@ module "wordpress_nginx_ecr_repository" {
   aws_accounts_allowed_to_pull = ["${data.aws_caller_identity.current.account_id}"]
 }
 
+# Wordpress PHP-FPM
+
 module "wordpress_php_fpm_ecr_repository" {
   source                       = "github.com/maxgio92/terraform-aws-ecr-repository?ref=1.2.0"
   name                         = "${var.app_name}/php-fpm"
   aws_accounts_allowed_to_pull = ["${data.aws_caller_identity.current.account_id}"]
 }
+
+# Wordpress PHP CLI
 
 module "wordpress_php_cli_ecr_repository" {
   source                       = "github.com/maxgio92/terraform-aws-ecr-repository?ref=1.2.0"
